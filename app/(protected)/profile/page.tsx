f276fec5afeff54e7fe53a02ac4d4a11047e95bf
ennalyc <ecsl2@cin.ufpe.br>
2026-04-02 10:31:42 -0300

    (fix): fix profile page minor ui errors

--- app/(protected)/profile/page.tsx
+++ app/(protected)/profile/page.tsx
@@ -4,7 +4,7 @@ import CollectionsSection from "@/app/components/sections/CollectionsSection"
 const ProfilePage = () => {
 
   return (
-    <div className="flex md:px-30 px-4 justify-center mt-8">
+    <div className="flex md:px-24 px-4 justify-center mt-8">
       <div className="w-full max-w-7xl">
           <h3 className="font-bold text-3xl">Your Profile</h3>
           <div className="flex flex-col md:flex-row gap-x-6">
